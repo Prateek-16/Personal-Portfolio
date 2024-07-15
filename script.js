72175f0e076876a61977cf034e32682cbcc4e02e
Prateek-16 <prateekverma54321@gmail.com>
2024-07-15 13:15:33 +0530

    UI Updation

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+var typeData = new Typed(".role", {
+  strings: [
+    "Full Stack Developer",
+    "Web Developer",
+    "UI-UX Designer",
+    "Backend Developer",
+    "Coder",
+  ],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+});  
